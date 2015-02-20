a59df980a7f3605961ec9e227ef2527763b99b70
Maxime Bellec <maxbellec@gmail.com>
2015-02-20 20:09:06 +0100

    ajout fonction pour resizer des images

---
+++ scripts/resize.py
@@ -0,0 +1,52 @@
+"""Script pour redimensionner un image avant de la publier sur le web. Les
+enregistre dans un sous-dossier nommé LQ
+ 
+Usage:
+  resize.py <nom_image_1> ... [--max_w <width>] [--max_h <height>] [-q <quality>]
+ 
+Options:
+  -h --help          C'est généré automatiquement.
+  --max_h=<valeur>  hauteur maximale
+  --max_w=<valeur>   largeur maximmale
+  -q qualite de sortie en jpg
+ 
+Copyright Maxime Bellec
+"""
+
+from docopt import docopt
+from PIL import Image
+import sys
+import os
+
+max_width, max_height = 1500, 1500
+
+# actual squaring function
+def resize(im_path, save_path):
+    im = Image.open(im_path)
+    ratio = im.size[0]/im.size[1]
+    if (ratio >=1):
+        # cas largeur plus grande
+        if im.size[0] > max_width:
+            im = im.resize((max_width, int(max_width/ratio)))
+    else:
+        if im.size[1] > max_height:
+            im = im.resize((max_width*ratio, max_height))
+    im.save(save_path)
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='0.1')
+    print(arguments)
+    
+    save_dir = os.path.join(os.getcwd(), 'LQ')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir) 
+
+    # getting the arguments
+    max_width = arguments['--max_w'] or 1500
+    max_height = arguments['--max_h'] or 1500
+    quality = arguments['-q'] or 60
+
+    # apply the resize function to every image
+    for image in arguments['<nom_image_1>']:
+        resize(os.path.join(os.getcwd(), image),
+               os.path.join(os.getcwd(), 'LQ', image))
